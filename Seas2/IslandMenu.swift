3631c69480f15475545e7100d8d677f47d428426
brianeromero <77758962+brianeromero@users.noreply.github.com>
2024-06-13 12:47:37 -0700

    maps working, image MF finder too big bu working, arrow open mat without background working but needs to be smaller and moved up. All working for now

--- Seas2/IslandMenu.swift
+++ Seas2/IslandMenu.swift
@@ -3,12 +3,10 @@
 //  Seas2
 //
 //  Created by Brian Romero on 6/7/24.
-
 import SwiftUI
 import CoreData
 import CoreLocation
 import MapKit
-
 
 struct MenuItem: Identifiable {
     let id = UUID()
@@ -31,65 +29,70 @@ struct IslandMenu: View {
 
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading, spacing: 20) {
-                Text("Main Menu")
-                    .font(.title)
-                    .bold()
-                    .padding(.top, 10)
+            ZStack {
+                // Display GIF as background
+                GIFView(name: "flashing")
 
-                ForEach(menuItems) { menuItem in
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text(menuItem.title)
-                            .font(.headline)
-                            .padding(.bottom, 20)
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Main Menu")
+                        .font(.title)
+                        .bold()
+                        .padding(.top, 10)
 
-                        if let subMenuItems = menuItem.subMenuItems {
-                            ForEach(subMenuItems, id: \.self) { subMenuItem in
-                                if subMenuItem == "Add New Gym" {
-                                    NavigationLink(destination: AddNewIsland()) {
-                                        Text(subMenuItem)
-                                            .foregroundColor(.blue)
+                    ForEach(menuItems) { menuItem in
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text(menuItem.title)
+                                .font(.headline)
+                                .padding(.bottom, 20)
+
+                            if let subMenuItems = menuItem.subMenuItems {
+                                ForEach(subMenuItems, id: \.self) { subMenuItem in
+                                    if subMenuItem == "Add New Gym" {
+                                        NavigationLink(destination: AddNewIsland()) {
+                                            Text(subMenuItem)
+                                                .foregroundColor(.blue)
+                                        }
+                                        .padding(.leading, 2)
+                                    } else if subMenuItem == "Update Existing" {
+                                        NavigationLink(destination: EditExistingIslandList()) {
+                                            Text(subMenuItem)
+                                                .foregroundColor(.blue)
+                                        }
+                                        .padding(.leading, 2)
+                                        .onTapGesture {
+                                            fetchIslandsNear(location: locationManager.userLocation ?? CLLocationCoordinate2D(latitude: 0, longitude: 0))
+                                        }
+                                    } else if subMenuItem == "Near Me (use current location)" {
+                                        NavigationLink(destination: AllIslandMapView()) {
+                                            Text(subMenuItem)
+                                                .foregroundColor(.blue)
+                                        }
+                                        .padding(.leading, 2)
+                                    } else {
+                                        Button(action: {
+                                            print("Selected: \(subMenuItem)")
+                                        }) {
+                                            Text(subMenuItem)
+                                                .foregroundColor(.blue)
+                                        }
+                                        .padding(.leading, 2)
                                     }
-                                    .padding(.leading, 2)
-                                } else if subMenuItem == "Update Existing" {
-                                    NavigationLink(destination: EditExistingIslandList()) {
-                                        Text(subMenuItem)
-                                            .foregroundColor(.blue)
-                                    }
-                                    .padding(.leading, 2)
-                                    .onTapGesture {
-                                        fetchIslandsNear(location: locationManager.userLocation ?? CLLocationCoordinate2D(latitude: 0, longitude: 0))
-                                    }
-                                } else if subMenuItem == "Near Me (use current location)" {
-                                    NavigationLink(destination: AllIslandMapView()) {
-                                        Text(subMenuItem)
-                                            .foregroundColor(.blue)
-                                    }
-                                    .padding(.leading, 2)
-                                } else {
-                                    Button(action: {
-                                        print("Selected: \(subMenuItem)")
-                                    }) {
-                                        Text(subMenuItem)
-                                            .foregroundColor(.blue)
-                                    }
-                                    .padding(.leading, 2)
                                 }
                             }
                         }
+                        .padding(.bottom, 20)
                     }
-                    .padding(.bottom, 20)
                 }
+                .padding(.horizontal, 20)
+                .navigationBarTitle("Welcome to Island Locator", displayMode: .inline)
+                .padding(.leading, -100)
             }
-            .padding(.horizontal, 20)
-            .navigationBarTitle("Welcome to Island Locator", displayMode: .inline)
-            .padding(.leading, -100)
+            .edgesIgnoringSafeArea(.all) // Make sure GIF fills the screen
         }
         .alert(isPresented: $showAlert) {
             Alert(title: Text("Location Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
         }
     }
-
 
     private func fetchIslandsNear(location: CLLocationCoordinate2D) {
         // Fetch islands near the given location using Core Data fetch request
---
+++ Seas2/SwiftGif.swift
@@ -0,0 +1,171 @@
+//
+//  SwiftGif.swift
+//  Seas2
+//
+//  Created by Brian Romero on 6/13/24.
+//
+
+import UIKit
+import SwiftUI
+import ImageIO
+
+extension UIImage {
+    public class func gifImageWithName(_ name: String) -> UIImage? {
+        guard let bundleURL = Bundle.main
+            .url(forResource: name, withExtension: "gif") else {
+            print("SwiftGif: This image named \"\(name)\" does not exist!")
+            return nil
+        }
+
+        guard let imageData = try? Data(contentsOf: bundleURL) else {
+            print("SwiftGif: Cannot turn image named \"\(name)\" into NSData")
+            return nil
+        }
+
+        return gifImageWithData(imageData)
+    }
+
+    public class func gifImageWithData(_ data: Data) -> UIImage? {
+        let source = CGImageSourceCreateWithData(data as CFData, nil)
+        return UIImage.animatedImageWithSource(source!)
+    }
+
+    class func delayForImageAtIndex(_ index: Int, source: CGImageSource!) -> Double {
+        var delay = 0.1
+
+        let cfProperties = CGImageSourceCopyPropertiesAtIndex(source, index, nil)
+        let gifProperties: CFDictionary = unsafeBitCast(
+            CFDictionaryGetValue(cfProperties,
+                                 Unmanaged.passUnretained(kCGImagePropertyGIFDictionary).toOpaque()),
+            to: CFDictionary.self)
+
+        var delayObject: AnyObject = unsafeBitCast(
+            CFDictionaryGetValue(gifProperties,
+                                 Unmanaged.passUnretained(kCGImagePropertyGIFUnclampedDelayTime).toOpaque()),
+            to: AnyObject.self)
+
+        if delayObject.doubleValue == 0 {
+            delayObject = unsafeBitCast(
+                CFDictionaryGetValue(gifProperties,
+                                     Unmanaged.passUnretained(kCGImagePropertyGIFDelayTime).toOpaque()),
+                to: AnyObject.self)
+        }
+
+        delay = delayObject as! Double
+
+        if delay < 0.1 {
+            delay = 0.1
+        }
+
+        return delay
+    }
+
+    class func gcdForPair(_ a: Int?, _ b: Int?) -> Int {
+        var a = a
+        var b = b
+        if b == nil || a == nil {
+            if b != nil {
+                return b!
+            } else if a != nil {
+                return a!
+            } else {
+                return 0
+            }
+        }
+
+        if a! < b! {
+            let c = a
+            a = b
+            b = c
+        }
+
+        var rest: Int
+        while true {
+            rest = a! % b!
+
+            if rest == 0 {
+                return b!
+            } else {
+                a = b
+                b = rest
+            }
+        }
+    }
+
+    class func gcdForArray(_ array: Array<Int>) -> Int {
+        if array.isEmpty {
+            return 1
+        }
+
+        var gcd = array[0]
+
+        for val in array {
+            gcd = UIImage.gcdForPair(val, gcd)
+        }
+
+        return gcd
+    }
+
+    class func animatedImageWithSource(_ source: CGImageSource) -> UIImage? {
+        let count = CGImageSourceGetCount(source)
+        var images = [CGImage]()
+        var delays = [Int]()
+
+        for i in 0..<count {
+            if let image = CGImageSourceCreateImageAtIndex(source, i, nil) {
+                images.append(image)
+            }
+
+            let delaySeconds = UIImage.delayForImageAtIndex(Int(i),
+                                                            source: source)
+            delays.append(Int(delaySeconds * 1000.0))
+        }
+
+        let duration: Int = {
+            var sum = 0
+
+            for val: Int in delays {
+                sum += val
+            }
+
+            return sum
+        }()
+
+        let gcd = gcdForArray(delays)
+        var frames = [UIImage]()
+
+        var frame: UIImage
+        var frameCount: Int
+
+        for i in 0..<count {
+            frame = UIImage(cgImage: images[Int(i)])
+            frameCount = Int(delays[Int(i)] / gcd)
+
+            for _ in 0..<frameCount {
+                frames.append(frame)
+            }
+        }
+
+        let animation = UIImage.animatedImage(with: frames,
+                                              duration: Double(duration) / 1000.0)
+
+        return animation
+    }
+}
+
+struct GIFView: UIViewRepresentable {
+    private let name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func makeUIView(context: Context) -> UIImageView {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage.gifImageWithName(name)
+        return imageView
+    }
+
+    func updateUIView(_ uiView: UIImageView, context: Context) {}
+}
